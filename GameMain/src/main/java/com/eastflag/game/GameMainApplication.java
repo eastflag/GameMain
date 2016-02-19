a8d30aa8f98578c769f69ee408a402c69cbb676a
eastflag <eastflag@gmail.com>
2016-02-19 19:26:43 +0900

    second commit

--- GameMain/src/main/java/com/eastflag/game/GameMainApplication.java
+++ GameMain/src/main/java/com/eastflag/game/GameMainApplication.java
@@ -1,9 +1,11 @@
-package com.eastflag;
+package com.eastflag.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+//@ImportResource("classpath:aaa/aaa.xml")
 public class GameMainApplication {
 
 	public static void main(String[] args) {
--- GameMain/src/main/java/com/eastflag/game/domain/HelloMessage.java
+++
@@ -1,8 +0,0 @@
-package com.eastflag.game.domain;
-
-import lombok.Data;
-
-@Data
-public class HelloMessage {
-	private String name;
-}
