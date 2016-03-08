601fe92e89afa6c9f6f052c68b4618f9754e081f
oyg <collme74@gmail.com>
2016-03-09 01:05:10 +0900

    # Websocket 서버 클라이언트 구현

--- GameMain/src/test/java/com/eastflag/GameMainApplicationTests.java
+++ GameMain/src/test/java/com/eastflag/GameMainApplicationTests.java
@@ -1,21 +1,77 @@
 package com.eastflag;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.eastflag.client.WebSocketClient;
 import com.eastflag.game.GameMainApplication;
+import com.eastflag.game.service.wordgame.vo.LoginRequestVO;
+import com.google.gson.Gson;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = GameMainApplication.class)
-@WebAppConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = GameMainApplication.class)
+//@WebAppConfiguration
 public class GameMainApplicationTests {
+	
+	public WebSocketClient client; 
+	
+	@Before
+	public void init() {
+		client = new WebSocketClient();
+		try {
+			client.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void auth_Login() {
+		// Make protocol
+		
+		LoginRequestVO loginReqVO = new LoginRequestVO();
+		loginReqVO.setId("collme74");
+		loginReqVO.setPassword("callme74");
+		
+		Gson gson = new Gson();
+		
+		String json = gson.toJson(loginReqVO);
+		
+		// Sends data line to the server.
+		Channel ch = client.getChannel();
+		ch.writeAndFlush(new TextWebSocketFrame(json));
+		
+		ChannelFuture closeFuture;
+		try {
+			closeFuture = ch.closeFuture().sync();
+			
+			if(closeFuture.isSuccess()) {
+				
+			}
+			else {
+				
+			}
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		fail("Not yet implemented");
 	}
 
 }
